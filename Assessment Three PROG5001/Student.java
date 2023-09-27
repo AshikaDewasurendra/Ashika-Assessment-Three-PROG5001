4e697a1171e7e4ff3d07118bfefb92e8ee125141
Ashika Dewasurendra <ashivv1@gmail.com>
2023-09-27 11:52:28 +0800

    updates

--- Assessment Three PROG5001/Student.java
+++ Assessment Three PROG5001/Student.java
@@ -3,6 +3,23 @@ import java.io.*;
 /**
  * Write a description of class StudentMarksManager here.
  *
- * @author (your name)
+ * @author (Ashika Dewasurendra)
  * @version (a version number or a date)
  */
+class Student{
+
+String lastName;
+String firstName;
+String studentID;
+double A1;
+double A2;
+double A3;
+
+
+
+
+
+
+
+
+}
